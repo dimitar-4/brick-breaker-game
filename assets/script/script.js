bd9e4fd8d1551fb5be522629f266b26052a781d1
Dimitar Cholakov <dcholakov4@gmail.com>
2022-02-09 15:57:06 +0100

    Build the field of bricks to break.

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,10 +6,26 @@ let dx = 2;
 let dy = -2;
 let ballRadius = 4;
 let paddleHeight = 5;
-let paddleWidth = 75;
+let paddleWidth = 65;
 let paddleX = (canvas.width-paddleWidth)/2;
 let rightPressed = false;
 let leftPressed = false;
+let brickRowCount = 4;
+let brickColumnCount = 6;
+let brickWidth = 35;
+let brickHeight = 6;
+let brickPadding = 8;
+let brickOffsetTop = 16;
+let brickOffsetLeft = 24;
+
+let bricks = [];
+for(let c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -48,9 +64,25 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#f5c253";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
 
